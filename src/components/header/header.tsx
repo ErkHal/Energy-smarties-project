d12739fb22d58f87c735d91810b4f692b290f19b
Unknown <tessam@metropolia.fi>
2019-11-04 17:03:08 +0200

    Fixed for merge
    Typo, deleted comments, added scrollview and added more padding

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { View, Text, StyleSheet, TextInput, Picker, Modal, TouchableHighlight } from "react-native";
+import { View, Text, StyleSheet, TextInput, Picker, Modal, TouchableHighlight, ScrollView } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 import { Dropdown } from 'react-native-material-dropdown';
-//import * as Font from 'expo-font';
  
  
 export default class Header extends React.Component {
@@ -12,16 +11,9 @@ export default class Header extends React.Component {
     setModalVisible(visible) {
         this.setState({modalVisible: visible});
     }
-    //Trying to get the fonts to work
-    /*async componentDidMount() {
-    await Font.loadAsync({
-      'montserrat-medium': require('../../assets/fonts/Montserrat-Medium.ttf'),
-    });
-    this.setState({ fontLoaded: true });
-  }*/
     render() {
         let sortingData = [{
-            value: 'Revelance',
+            value: 'Relevance',
           }, {
             value: 'Country',
           }, {
@@ -45,26 +37,27 @@ export default class Header extends React.Component {
                 </View>
                 <Modal animationType={"slide"} transparent={false} visible={this.state.modalVisible}>
                     <View style={styles.modal}>
-                        <View style={styles.firstRowModal}>
-                            <Text style={styles.appTitle}>Greener Apps</Text>
-                            <Ionicons style={styles.icon}name="ios-close" size={32} onPress={() => {
-                            this.setModalVisible(!this.state.modalVisible)
-                            }}/>
-                        </View>
-                        <Text style={styles.h1}>Total score</Text>
-                        <Text style={styles.modalText}>Weighted sum of individual scores scaled to 0-10: energy score (weight = 4), company score (weight = 2), city score (weight = 2) and country score (weight = 2). </Text>
-                        <Text style={styles.h1}>Energy score</Text>
-                        <Text style={styles.modalText}>Battery consumption (mAh) of an app measured by Greenspector®. Energy scores can be compared only within a category. Lower consumption results in higher score.</Text>
-                        <Text style={styles.h1}>Company score</Text>
-                        <Text style={styles.modalText}>Company-derived carbon footprint per employee. Lower carbon dioxide emissions result in higher score.</Text>
-                        <Text style={styles.h1}>City score</Text>
-                        <Text style={styles.modalText}>Helsinki, Espoo and Vantaa: Carbon footprint for 2018 from Helsinki Region Infoshare (https://hri.fi/data/en_GB/dataset/paakaupunkiseudun-kasvihuonekaasupaastot/resource/665bf9b6-5627-4763-935c-9c9723c4b8b8)
-Other cities: Moran, D., Kanemoto K; Jiborn, M., Wood, R., Többen, J., and Seto, K.C. (2018) Carbon footprints of 13,000 cities. Environmental Research Letters DOI: 10.1088/1748-9326/aac72a.
-Lower carbon dioxide emissions per citizen result in higher score.</Text>
-                        <Text style={styles.h1}>Country score</Text>
-                        <Text style={styles.modalText}>Data from the 2018 Global Green Economy Index (GGEI) published by Dual Citizen LLC (https://dualcitizeninc.com/global-green-economy-index/). 
-Higher index value results in higher score. 
-</Text>
+                        <ScrollView>
+                            <View style={styles.firstRowModal}>
+                                <Text style={styles.appTitle}>Greener Apps</Text>
+                                <Ionicons style={styles.icon}name="ios-close" size={32} onPress={() => {
+                                this.setModalVisible(!this.state.modalVisible)
+                                }}/>
+                            </View>
+                            <Text style={styles.h1}>Total score</Text>
+                            <Text style={styles.modalText}>Weighted sum of individual scores scaled to 0-10: energy score (weight = 4), company score (weight = 2), city score (weight = 2) and country score (weight = 2). </Text>
+                            <Text style={styles.h1}>Energy score</Text>
+                            <Text style={styles.modalText}>Battery consumption (mAh) of an app measured by Greenspector®. Energy scores can be compared only within a category. Lower consumption results in higher score.</Text>
+                            <Text style={styles.h1}>Company score</Text>
+                            <Text style={styles.modalText}>Company-derived carbon footprint per employee. Lower carbon dioxide emissions result in higher score.</Text>
+                            <Text style={styles.h1}>City score</Text>
+                            <Text style={styles.modalText}>Helsinki, Espoo and Vantaa: Carbon footprint for 2018 from Helsinki Region Infoshare (https://hri.fi/data/en_GB/dataset/paakaupunkiseudun-kasvihuonekaasupaastot/resource/665bf9b6-5627-4763-935c-9c9723c4b8b8)
+                            Other cities: Moran, D., Kanemoto K; Jiborn, M., Wood, R., Többen, J., and Seto, K.C. (2018) Carbon footprints of 13,000 cities. Environmental Research Letters DOI: 10.1088/1748-9326/aac72a.
+                            Lower carbon dioxide emissions per citizen result in higher score.</Text>
+                            <Text style={styles.h1}>Country score</Text>
+                            <Text style={styles.modalText}>Data from the 2018 Global Green Economy Index (GGEI) published by Dual Citizen LLC (https://dualcitizeninc.com/global-green-economy-index/). 
+                            Higher index value results in higher score. </Text>
+                        </ScrollView>
                     </View>
                 </Modal>    
             </View>
@@ -92,7 +85,7 @@ Higher index value results in higher score.
     header: {
         backgroundColor: '#FAFAFA',
         width: '100%',
-        height: 200,
+        height: '35%',
         display: 'flex',
         alignItems: "center",
     },
@@ -100,8 +93,8 @@ Higher index value results in higher score.
         display: 'flex',
         alignItems: "center",
         flexDirection: 'row',
-        paddingTop: 40,
-        paddingBottom: 10
+        paddingTop: '8%',
+        paddingBottom: '8%'
     },
     secondRow: {
         display: 'flex',
@@ -117,7 +110,6 @@ Higher index value results in higher score.
         textAlign: 'center',
         color: '#2CBC52',
         fontSize: 30,
-        //fontFamily: 'montserrat-medium'
     }, 
     icon: {
         color: '#707070',
@@ -164,4 +156,3 @@ Higher index value results in higher score.
         textAlign: 'justify'
     }
   })
-
