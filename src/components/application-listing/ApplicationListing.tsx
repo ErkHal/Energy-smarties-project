9650df816c513584479a531999c3d6f6eaf34c04
Erkki Halinen <erkki-eemeli.halinen@cgi.com>
2019-11-04 17:33:05 +0200

    temp commit

--- src/components/application-listing/ApplicationListing.tsx
+++ src/components/application-listing/ApplicationListing.tsx
@@ -11,23 +11,36 @@ export const ApplicationListing = (props: Props) => {
         <View style={styles.listing}>
             <Text style={styles.appName}>{props.appInfo.name}</Text>
             <Text style={styles.appCategory}>{props.appInfo.category}</Text>
-            <Text>City: {props.appInfo.city}</Text>
+            <Text style={styles.appCity}>City: {props.appInfo.city}</Text>
+            <Text style={styles.appCountry}>Country: {props.appInfo.country}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     listing: {
-        height: 100,
         borderBottomWidth: 1,
-        borderBottomColor: '#505050'
+        borderBottomColor: '#505050',
+        display: 'flex',
+        flexDirection: 'column'
     },
     appName: {
         color: 'black',
+        width: '100%',
         fontSize: 34
     },
     appCategory: {
         color: 'darkgray',
+        width: '100%',
         fontSize: 15
+    },
+    appCity: {
+        marginTop: 15,
+        fontSize: 20,
+        width: '50%'
+    },
+    appCountry: {
+        width: '50%',
+        fontSize: 20,
     }
 })
